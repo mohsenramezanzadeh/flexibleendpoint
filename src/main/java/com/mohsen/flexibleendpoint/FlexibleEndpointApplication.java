163e500bb84ac9d21cc86c12b0da401a45095667
Mohsen Ramezanzadeh <mohsen.ramezanzadeh70@gmail.com>
2024-11-02 23:41:00 +0100

    add base

---
+++ src/main/java/com/mohsen/flexibleendpoint/FlexibleEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.mohsen.flexibleendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexibleEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexibleEndpointApplication.class, args);
+	}
+
+}
